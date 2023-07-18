95872f6faf9385eb6b56054a95308b27404085f0
bpuchala <bpuchala@users.noreply.github.com>
2023-07-18 02:44:48 -0400

    doc updates

--- python/doc/conf.py
+++ python/doc/conf.py
@@ -116,13 +116,22 @@ html_theme = "pydata_sphinx_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+
+html_logo = "_static/small_logo.svg"
+html_theme_options = {
+    "logo": {
+        "text": "libcasm-global",
+    }
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_favicon = "_static/small_logo.svg"
+html_css_files = [
+    "css/custom.css",
+]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
