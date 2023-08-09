57c5d80eceece7ec1f221cf9284e8123b0beb5f9
bpuchala <bpuchala@users.noreply.github.com>
2023-08-09 15:03:44 -0400

    version 2.0.2, redo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="libcasm-global",
-    version="2.0.1",
+    version="2.0.2",
     packages=["libcasm", "libcasm.casmglobal", "libcasm.counter"],
     package_dir={"": "python"},
     cmake_install_dir="python/libcasm",
