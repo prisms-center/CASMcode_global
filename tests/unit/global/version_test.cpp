888f24252240745145613034e0fd2b4d0329dfaf
bpuchala <bpuchala@users.noreply.github.com>
2026-09-23 10:23:02 -0400

    Fix version_test.cpp patch version check for 2.4.2

--- tests/unit/global/version_test.cpp
+++ tests/unit/global/version_test.cpp
@@ -23,7 +23,7 @@ TEST(VersionTest, Test1) {
   EXPECT_EQ(v_match.size(), 6);
   EXPECT_EQ(v_match[1].str(), "2");
   EXPECT_EQ(v_match[2].str(), "4");
-  EXPECT_EQ(v_match[3].str(), "1");
+  EXPECT_EQ(v_match[3].str(), "2");
   EXPECT_EQ(v_match[4].str(), "");
 
   std::cout << "CASM::version(): " << CASM::version() << std::endl;
